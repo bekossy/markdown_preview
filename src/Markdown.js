a9fea13d8105f17e61ca9b1af2cdf1c3e9f70569
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-09-24 22:18:40 +0100

    implements markdown logic

--- src/Markdown.js
+++ src/Markdown.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 const Markdown = () => {
-  return <>Markdown</>;
+  const [text, setText] = useState("# MarkDown Preview");
+  return (
+    <>
+      <textarea
+        name="markdown"
+        id="markdown"
+        cols="30"
+        rows="10"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <ReactMarkdown className="preview">{text}</ReactMarkdown>
+    </>
+  );
 };
 
 export default Markdown;
