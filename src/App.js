22204bd56ea13db88b456799e9ac6cdb0281a32b
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-09-24 15:04:09 +0100

    creates Markdown.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Markdown from "./Markdown";
 
 function App() {
-  return <div className="App">React App</div>;
+  return (
+    <div className="App">
+      <Markdown />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Markdown.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Markdown = () => {
+  return <>Markdown</>;
+};
+
+export default Markdown;
